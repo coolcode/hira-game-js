cfe4ac4a3d0ad397833332469d7658289a0817d2
coolcode <coolcode@live.com>
2023-09-09 17:03:49 +0800

    katakana

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Game from './Game'
 
 const HIRAGANA = {
   'あ': 'a', 'い': 'i', 'う': 'u', 'え': 'e', 'お': 'o',
@@ -14,194 +16,67 @@ const HIRAGANA = {
   'ん': 'n',
 }
 
-const HIRAGANA_LEN = Object.keys(HIRAGANA).length
-
-const HiraganaGame = () => {
-  const [currentHiragana, setCurrentHiragana] = useState('')
-  const [lastHiragana, setLastHiragana] = useState('')
-  const [userInput, setUserInput] = useState('')
-  const [correctCount, setCorrectCount] = useState(0)
-  const [wrongCount, setWrongCount] = useState(0)
-  const [showCorrectAnswer, setShowCorrectAnswer] = useState(false)
-  const [hiraganaErrors, setHiraganaErrors] = useState({})
-  const [learnedSymbols, setLearnedSymbols] = useState(new Set())
-
-  useEffect(() => {
-    // Load errors from localStorage on component mount
-    const storedErrors = JSON.parse(localStorage.getItem('hiraganaErrors')) || {}
-    updateHiraganaErrors(storedErrors)
-
-    // Randomly select a Hiragana character
-    const randomHiragana = getRandomHiragana(storedErrors)
-    setCurrentHiragana(randomHiragana)
-  }, [])
-
-  const getRandomHiragana = (errors) => {
-    const errorKeys = Object.keys(errors)
-    const hiraganaCharacters = errorKeys.length > 0 && Math.floor(Math.random() * 100) < 10 * errorKeys.length ? errorKeys : Object.keys(HIRAGANA)
-    const randomIndex = Math.floor(Math.random() * hiraganaCharacters.length)
-    return hiraganaCharacters[randomIndex]
-  }
-
-  const checkUserInput = () => {
-    if (userInput === HIRAGANA[currentHiragana]) {
-      // User input is correct 
-      setShowCorrectAnswer(true)
-
-      // Decrease the error count for the current Hiragana (if it's positive)
-      if (hiraganaErrors[currentHiragana] > 0) {
-        const updatedErrors = { ...hiraganaErrors }
-        updatedErrors[currentHiragana] -= 1
-        updateHiraganaErrors(updatedErrors)
-      } else {
-        if (correctCount < HIRAGANA_LEN) {
-          setCorrectCount(correctCount + 1)
-        }
-      }
-      learnedSymbols.add(currentHiragana)
-      setLearnedSymbols(learnedSymbols) 
-    } else {
-      // User input is incorrect 
-      setShowCorrectAnswer(false)
-
-      // Increase the error count for the current Hiragana
-      const updatedErrors = { ...hiraganaErrors }
-      updatedErrors[currentHiragana] = (updatedErrors[currentHiragana] || 0) + 1
-      updateHiraganaErrors(updatedErrors)
-      if(learnedSymbols.has(currentHiragana)){
-        learnedSymbols.delete(currentHiragana)
-        setLearnedSymbols(learnedSymbols) 
-      }
-    }
-
-    setLastHiragana(currentHiragana)
-    // Play the audio pronunciation of the Hiragana character (correct one)
-    playAudio(currentHiragana)
-
-    // Clear user input
-    setUserInput('')
-
-    // Move to the next Hiragana character
-    const randomHiragana = getRandomHiragana(hiraganaErrors)
-    setCurrentHiragana(randomHiragana)
-  }
-
-  const playAudio = (hiragana) => {
-    //play audio
-    const audio = new Audio(`audio/${HIRAGANA[hiragana]}.mp3`) // Get the audio file path based on the Hiragana character
-    audio.play()
-  }
-
-  const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
-      checkUserInput()
-    }
-  }
-
-  const calculateAccuracy = () => {
-    const accuracy = (correctCount * 100 / HIRAGANA_LEN)
-    return accuracy.toFixed() + '%'
-  }
-
-  const updateHiraganaErrors = (errors) => {
-    const entries = Object.entries(errors).filter(([, value]) => value !== 0)
-    setWrongCount(entries.length)
-
-    const sortedEntries = entries.sort((a, b) => b[1] - a[1])
-    const sortedErrors = Object.fromEntries(sortedEntries)
-
-    setHiraganaErrors(sortedErrors)
-    // Save the updated errors to localStorage
-    localStorage.setItem('hiraganaErrors', JSON.stringify(sortedErrors))
-  }
-
-  const reset = () => {
-    setLastHiragana('')
-    updateHiraganaErrors({})
-    setCorrectCount(0)
-    setWrongCount(0)
-    setLearnedSymbols(new Set())
-    setShowCorrectAnswer(false)
-    setUserInput('')
-  }
-
-  return (<div className="flex flex-col items-center">
-    <h1 className="text-9xl mb-10">{currentHiragana}</h1>
-    <div className="bg-white p-6">
-      <label className='mr-2'>Romaji:</label>
-      <input
-        type="text"
-        className="border border-gray-300 mr-2 p-2 text-xl mb-4"
-        value={userInput}
-        onChange={(e) => setUserInput(e.target.value)}
-        placeholder=""
-        onKeyDown={handleKeyPress}
-      />
-      <button
-        className="bg-green-500 text-white mr-2 py-2 px-4 text-xl hover:bg-green-700"
-        onClick={checkUserInput}
-      >
-        Check
-      </button>
-      {
-        lastHiragana && (showCorrectAnswer ? (
-          <p className="text-green-500 text-2xl mt-4">✅ {lastHiragana} - {HIRAGANA[lastHiragana]} </p>
-        ) : (
-          <p className="text-red-500 text-2xl mt-4">❌ {lastHiragana} - {HIRAGANA[lastHiragana]} </p>
-        ))
-      }
-      <hr />
-      <p className="text-l mt-4">Correct: {correctCount}, Wrong: {wrongCount}, Accuracy: {calculateAccuracy()} </p>
-      {wrongCount > 0 && (<h3 className="text-l mt-4 text-red-500">Errors:</h3>)}
-      <ul className="text-red-500">
-        {Object.entries(hiraganaErrors).map(([hiragana, errorCount]) => (
-          <li key={hiragana}>{hiragana}: {errorCount}</li>
-        ))}
-      </ul>
-
-      <button
-        className="bg-amber-500 text-white mt-4 py-2 px-4 text-xl hover:bg-amber-700"
-        onClick={reset}
-      >
-        Reset
-      </button>
-    </div>
-    <a
-      className="mt-4 link"
-      href="https://github.com/coolcode/hira-game-js"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Source Code
-    </a>
-  </div>
-  )
+const KATAKANA = {
+  'ア': 'a', 'イ': 'i', 'ウ': 'u', 'エ': 'e', 'オ': 'o',
+  'カ': 'ka', 'キ': 'ki', 'ク': 'ku', 'ケ': 'ke', 'コ': 'ko',
+  'サ': 'sa', 'シ': 'shi', 'ス': 'su', 'セ': 'se', 'ソ': 'so',
+  'タ': 'ta', 'チ': 'chi', 'ツ': 'tsu', 'テ': 'te', 'ト': 'to',
+  'ナ': 'na', 'ニ': 'ni', 'ヌ': 'nu', 'ネ': 'ne', 'ノ': 'no',
+  'ハ': 'ha', 'ヒ': 'hi', 'フ': 'fu', 'ヘ': 'he', 'ホ': 'ho',
+  'マ': 'ma', 'ミ': 'mi', 'ム': 'mu', 'メ': 'me', 'モ': 'mo',
+  'ヤ': 'ya', 'ユ': 'yu', 'ヨ': 'yo',
+  'ラ': 'ra', 'リ': 'ri', 'ル': 'ru', 'レ': 're', 'ロ': 'ro',
+  'ワ': 'wa', 'ヲ': 'wo',
+  'ン': 'n',
 }
+
 
 function App() {
   return (
-    <div class="bg-gray-200">
-      <header class="bg-white shadow">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-            &nbsp;&nbsp;/\_/\
-          </h1>
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">( o.o )
-          </h1>
-          <h1 class="text-3xl font-bold tracking-tight text-gray-900">
-            あいうえお Hiragana Learning Game
-          </h1>
-        </div>
-      </header>
-      <main>
-        <div class="h-screen mx-auto max-w-8xl py-6 sm:px-6 lg:px-8">
-          <HiraganaGame />
-        </div>
-      </main>
-      <footer class="bg-white shadow flex flex-col items-center justify-center">
+    <Router>
+      <div className="bg-gray-100 ">
+        <header className="bg-white shadow">
+          <div className="container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+            <nav className="flex items-center justify-between">
+              <div className="flex-shrink-0">
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900 px-2">
+                  /\_/\
+                </h1>
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">( o.o )</h1>
+                <h1 className="text-3xl font-bold tracking-tight text-gray-900">あいうえお</h1>
+                <h2 className="text-xl">Learning Game</h2>
+              </div>
+              <div className="block">
+                <ul className="">
+                  {/* <li>
+                    <Link to="/" className="text-gray-900 hover:text-blue-500 transition duration-300">Home</Link>
+                  </li> */}
+                  <li>
+                    <Link to="/hiragana" className="text-gray-900 hover:text-blue-500 transition duration-300">HIRAGANA</Link>
+                  </li>
+                  <li>
+                    <Link to="/katakana" className="text-gray-900 hover:text-blue-500 transition duration-300">KATAKANA</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </header>
+        <main>
+          <div className="h-screen mx-auto max-w-8xl py-6 sm:px-6 lg:px-8">
+            {/* <Game symbols={HIRAGANA} /> */}
+            <Routes>
+              <Route path="/" exact element={<Game symbols={HIRAGANA} symbolType='HIRAGANA' />} />
+              <Route path="/hiragana" element={<Game symbols={HIRAGANA} symbolType='HIRAGANA' />} />
+              <Route path="/katakana" element={<Game symbols={KATAKANA} symbolType='KATAKANA' />} />
+            </Routes>
+          </div>
+        </main>
+        <footer className="bg-white shadow flex flex-col items-center justify-center">
 
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </Router>
   )
 }
 
---
+++ src/Game.js
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from 'react'
+
+const Game = ({ symbols, symbolType = 'HIRAGANA' }) => {
+    const numberOfSymbols = Object.keys(symbols).length
+    const storageKey = `FailedSymbols_${symbolType}`
+
+    const [currentSymbol, setCurrentSymbol] = useState('')
+    const [lastSymbol, setLastSymbol] = useState('')
+    const [userInput, setUserInput] = useState('')
+    const [correctCount, setCorrectCount] = useState(0)
+    const [wrongCount, setWrongCount] = useState(0)
+    const [showCorrectAnswer, setShowCorrectAnswer] = useState(false)
+    const [failedSymbols, setFailedSymbols] = useState({})
+    const [learnedSymbols, setLearnedSymbols] = useState(new Set())
+
+    useEffect(() => {
+        reset()
+        // Load errors from localStorage on component mount
+        const storedErrors = JSON.parse(localStorage.getItem(storageKey)) || {}
+        updateFailedSymbols(storedErrors)
+
+        // Randomly select a Symbol character
+        const randomSymbol = getRandomSymbol(storedErrors)
+        setCurrentSymbol(randomSymbol)
+    }, [symbolType])
+
+    const getRandomSymbol = (errors) => {
+        const errorKeys = Object.keys(errors)
+        const hiraganaCharacters = errorKeys.length > 0 && Math.floor(Math.random() * 100) < 10 * errorKeys.length ? errorKeys : Object.keys(symbols)
+        const randomIndex = Math.floor(Math.random() * hiraganaCharacters.length)
+        return hiraganaCharacters[randomIndex]
+    }
+
+    const checkUserInput = () => {
+        if (userInput === symbols[currentSymbol]) {
+            // User input is correct 
+            setShowCorrectAnswer(true)
+
+            // Decrease the error count for the current Symbol (if it's positive)
+            if (failedSymbols[currentSymbol] > 0) {
+                const updatedErrors = { ...failedSymbols }
+                updatedErrors[currentSymbol] -= 1
+                updateFailedSymbols(updatedErrors)
+            } else {
+                if (correctCount < numberOfSymbols) {
+                    setCorrectCount(correctCount + 1)
+                }
+            }
+            learnedSymbols.add(currentSymbol)
+            setLearnedSymbols(learnedSymbols)
+        } else {
+            // User input is incorrect 
+            setShowCorrectAnswer(false)
+
+            // Increase the error count for the current Symbol
+            const updatedErrors = { ...failedSymbols }
+            updatedErrors[currentSymbol] = (updatedErrors[currentSymbol] || 0) + 1
+            updateFailedSymbols(updatedErrors)
+            if (learnedSymbols.has(currentSymbol)) {
+                learnedSymbols.delete(currentSymbol)
+                setLearnedSymbols(learnedSymbols)
+            }
+        }
+
+        setLastSymbol(currentSymbol)
+        // Play the audio pronunciation of the Symbol character (correct one)
+        playAudio(currentSymbol)
+
+        // Clear user input
+        setUserInput('')
+
+        // Move to the next Symbol character
+        const randomSymbol = getRandomSymbol(failedSymbols)
+        setCurrentSymbol(randomSymbol)
+    }
+
+    const playAudio = (hiragana) => {
+        //play audio
+        const audio = new Audio(`audio/${symbols[hiragana]}.mp3`) // Get the audio file path based on the Symbol character
+        audio.play()
+    }
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            checkUserInput()
+        }
+    }
+
+    const calculateAccuracy = () => {
+        const accuracy = (correctCount * 100 / numberOfSymbols)
+        return accuracy.toFixed() + '%'
+    }
+
+    const updateFailedSymbols = (errors) => {
+        const entries = Object.entries(errors).filter(([, value]) => value !== 0)
+        setWrongCount(entries.length)
+
+        const sortedEntries = entries.sort((a, b) => b[1] - a[1])
+        const sortedErrors = Object.fromEntries(sortedEntries)
+
+        setFailedSymbols(sortedErrors)
+        // Save the updated errors to localStorage
+        localStorage.setItem(storageKey, JSON.stringify(sortedErrors))
+    }
+
+    const reset = (clearFailedSymbol = false) => {
+        setLastSymbol('')
+        setCorrectCount(0)
+        setWrongCount(0)
+        setLearnedSymbols(new Set())
+        setShowCorrectAnswer(false)
+        setUserInput('')
+        if (clearFailedSymbol) {
+            updateFailedSymbols({})
+        }
+    }
+
+    return (<div className="flex flex-col items-center">
+        <h1 className="text-9xl mb-10">{currentSymbol}</h1>
+        <div className="bg-white p-6">
+            <label className='mr-2'>Romaji:</label>
+            <input
+                type="text"
+                className="border border-gray-300 mr-2 p-2 text-xl mb-4"
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+                placeholder=""
+                onKeyDown={handleKeyPress}
+            />
+            <button
+                className="bg-green-500 text-white mr-2 py-2 px-4 text-xl hover:bg-green-700"
+                onClick={checkUserInput}
+            >
+                Check
+            </button>
+            {
+                lastSymbol && (showCorrectAnswer ? (
+                    <p className="text-green-500 text-2xl mt-4">✅ {lastSymbol} - {symbols[lastSymbol]} </p>
+                ) : (
+                    <p className="text-red-500 text-2xl mt-4">❌ {lastSymbol} - {symbols[lastSymbol]} </p>
+                ))
+            }
+            <hr />
+            <p className="text-l mt-4">Correct: {correctCount}, Wrong: {wrongCount}, Accuracy: {calculateAccuracy()} </p>
+            {wrongCount > 0 && (<h3 className="text-l mt-4 text-red-500">Errors:</h3>)}
+            <ul className="text-red-500">
+                {Object.entries(failedSymbols).map(([hiragana, errorCount]) => (
+                    <li key={hiragana}><span>{hiragana}: {errorCount}</span> <a href="javascript:void(0);" onClick={()=>playAudio(hiragana)} className="text-green-500 px-2 py-3">🎵</a></li>
+                ))}
+            </ul>
+
+            <button
+                className="bg-amber-500 text-white mt-4 py-2 px-4 text-xl hover:bg-amber-700"
+                onClick={() => reset(true)}
+            >
+                Reset
+            </button>
+        </div>
+        <a
+            className="mt-4 link"
+            href="https://github.com/coolcode/hira-game-js"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            Source Code
+        </a>
+    </div>
+    )
+}
+
+export default Game
